ed4d95bbe5c2219d884072764799e6bcc7736750
Muhammad Rashid <mrashidramzan1996@gmail.com>
2025-04-22 17:01:43 +0500

    Set basics

---
+++ src/app/(root)/interviews/page.tsx
@@ -0,0 +1,10 @@
+import InterviewGrid from "@/components/interview/listing/InterviewGrid";
+import InterviewLayout from "@/components/interview/listing/InterviewLayout";
+
+export default function InterviewsPage() {
+  return (
+    <InterviewLayout>
+      <InterviewGrid />
+    </InterviewLayout>
+  );
+}
