e8f60acfbcd067fee7e77adac3d84b9c3ac26a6e
dev-mian-zubair <mianmuhammadzubair786@gmail.com>
2025-04-25 16:26:35 +0500

    WIP

--- src/components/call/AgentCard.tsx
+++ src/components/call/AgentCard.tsx
@@ -64,28 +64,28 @@ const AgentCard = ({
   }, [isSpeaking]);
 
   return (
-    <div className="absolute top-12 left-12 z-30">
+    <div className="absolute top-4 left-4 sm:top-6 sm:left-6 z-30 w-60 sm:w-72">
       <Card
-        className={`relative overflow-hidden rounded-medium border-none shadow-md w-72 bg-gradient-to-tl ${randomGradient}`}
+        className={`relative overflow-hidden rounded-lg border-none shadow-md bg-gradient-to-tl ${randomGradient}`}
       >
         <div className="absolute inset-0 bg-gradient-to-t z-10" />
-        <CardBody className="flex flex-col items-center justify-center p-4 z-20 relative h-38">
+        <CardBody className="flex flex-col items-center justify-center p-3 sm:p-4 z-20 relative h-32 sm:h-38">
           <Chip
-            className="absolute top-2 left-2 size-5 text-white"
+            className="absolute top-1 left-1 text-white text-xs"
             color={chipInfo[0] as any}
             size="sm"
           >
             {chipInfo[1]}
           </Chip>
           <div
-            className={`rounded-full p-4 ${
+            className={`rounded-full p-3 sm:p-4 ${
               isSpeaking
                 ? `animate-pulse ${randomSpeakingColors.pulseColor}`
                 : ""
             }`}
           >
             <Avatar
-              className={`w-12 h-12 border-2 rounded-full ${
+              className={`w-10 h-10 sm:w-12 sm:h-12 border-2 rounded-full ${
                 isSpeaking
                   ? randomSpeakingColors.borderColor
                   : "border-transparent"
@@ -94,7 +94,7 @@ const AgentCard = ({
               src="https://i.pravatar.cc/150?u=a04258114e29026708c"
             />
           </div>
-          <span className="text-xs font-medium absolute bottom-2 left-2 text-black/80">
+          <span className="text-xs font-medium absolute bottom-1 left-1 text-black/80">
             Hana
           </span>
         </CardBody>
