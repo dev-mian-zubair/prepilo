8d1c8f30422d652d41d107573c23ebf2539ef5b2
dev-mian-zubair <mianmuhammadzubair786@gmail.com>
2025-06-05 18:04:28 +0500

    Removed the discover page

--- src/app/app/discover/page.tsx
+++
@@ -1,9 +0,0 @@
-import DiscoverTemplateGrid from "@/components/discover/listing/DiscoverTemplateGrid";
-
-export default function DiscoverPage() {
-  return (
-    <div>
-      <DiscoverTemplateGrid />
-    </div>
-  );
-}
